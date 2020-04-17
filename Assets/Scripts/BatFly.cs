83bc09853ac21e6ce023dbf26b4d5cba169e944e
gretmott <gretmott@gmail.com>
2020-04-16 21:54:41 -0400

    Collision

--- Assets/Scripts/BatFly.cs
+++ Assets/Scripts/BatFly.cs
@@ -6,6 +6,9 @@ public class BatFly : MonoBehaviour
 {
     public Rigidbody2D rb2D;
     public float moveSpeed;
+    public Animator myanim;
+    public bool gold;
+    public AudioSource hitsound;
 
     // Start is called before the first frame update
     void Start()
@@ -22,10 +25,39 @@ public class BatFly : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player")
+
+        if(collision.tag == "Player" && gold == false)
+        {
+            StartCoroutine("Hit");
+        }
+
+        if (collision.tag == "Player" && gold == true)
         {
             //collect the gold and add to gold score
+            collision.GetComponent<PlayerController>().gold += 1;
+
             MF_AutoPool.Despawn(this.gameObject); //this would despawn the gold
+
         }
+
+        if (collision.tag == "ball" && gold == false)
+
+        {
+            StartCoroutine("Hit");
+        }
+
+         
+
     }
+
+    IEnumerator Hit ()
+    {
+        myanim.SetBool("Hit", true);
+        hitsound.Play();
+        yield return new WaitForSeconds(1f);
+        myanim.SetBool("Hit", false);
+        MF_AutoPool.Despawn(this.gameObject);
+    }
+
+
 }
